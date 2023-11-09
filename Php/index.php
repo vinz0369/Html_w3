98d70349b5a73f73862bae8a1dbffdfad9bda6b7
vinz0369 <vinhht2k3@gmail.com>
2023-11-09 21:57:46 +0700

    slide 13

---
+++ Php/index.php
@@ -0,0 +1,8 @@
+<?php
+    echo "MY FIRST PHP FILE";
+    echo "<h3>Hello World!</h3>";
+    echo"<select>";
+    echo "<option>1</option>";
+    echo "<option>2</option>";
+    echo "</select>";
+?>
