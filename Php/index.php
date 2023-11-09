ecd12cf5f265935d73825534645b98b5c8064eb1
vinz0369 <vinhht2k3@gmail.com>
2023-11-09 21:59:27 +0700

    slide 19

--- Php/index.php
+++ Php/index.php
@@ -1,8 +1,17 @@
-<?php
-    echo "MY FIRST PHP FILE";
-    echo "<h3>Hello World!</h3>";
-    echo"<select>";
-    echo "<option>1</option>";
-    echo "<option>2</option>";
-    echo "</select>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php 
+    echo"MY FIRST PHP FILE";
+    ?>
+    <h3> Hello world !</h3>
+    <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+    </select>
+</body>
+</html>
