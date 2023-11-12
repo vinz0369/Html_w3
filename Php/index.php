761b8303614eb2a450ddc5fd20ce979f66042941
vinz0369 <vinhht2k3@gmail.com>
2023-11-12 21:54:56 +0700

    thuchanh_GET

--- Php/index.php
+++ Php/index.php
@@ -1,18 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<meta charset ="utf-8">
-<title></title>
-<body>
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email address is : <?php echo $_POST["email"];?>
+    <meta charset="utf-8">
+    <title>Confirm</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
 
+        .confirm {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        p {
+            margin: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="confirm">
     
+        <h2>Xác nhận thông tin</h2>
+        <p>Tên của bạn là: <?php echo $_GET["name"];//ten ?></p>
+        <p>Bạn <?php echo $_GET["age"]; //tuoi?> tuổi</p>
+        <p>Địa chỉ của bạn là: <?php echo $_GET["add"]; //dia chi?></p>
+        <p>Địa chỉ email: <?php echo $_GET["email"]; //email?></p>
+        <p>Số điện thoại: <?php echo $_GET["phonenumber"]; //sodienthoai?></p>
     
+    </div>
 </body>
 </html>
