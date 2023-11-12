0dbc4273864d6c51c5ac8619e1a21ed3853fa1c0
vinz0369 <vinhht2k3@gmail.com>
2023-11-12 20:55:24 +0700

    6-phppostform

--- Php/index.php
+++ Php/index.php
@@ -9,8 +9,8 @@
 <meta charset ="utf-8">
 <title></title>
 <body>
-    Welcome <?php echo $_GET["name"]; ?><br>
-    Your email address is : <?php echo $_GET["email"];?>
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email address is : <?php echo $_POST["email"];?>
     
 </body>
 </html>
