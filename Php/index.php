74424e79595f32f9746e6afeda24629c2c0349a1
vinz0369 <vinhht2k3@gmail.com>
2023-11-14 16:11:17 +0700

    9-ifpractiseresult.php

--- Php/index.php
+++ Php/index.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-	<h1>Currency Converter</h1>
-	<form method="get">
-		<label for="amount">Amount:</label>
-		<input type="number"  name="amount"><br><br>
-		<label for="currency">Select currency:</label>
-		<select  name="currency">
-			<option value="USD">USD</option>
-			<option value="AUD">AUD</option>
-			<option value="JPY">JPY</option>
-			<option value="EUR">EUR</option>
-		</select><br><br>
-		<input type="submit" value="Convert">
-	</form>
+
 <?php
     $USD =23000;
     $AUD =17000;
@@ -54,5 +33,3 @@
     }
     echo"VND";
     ?>
-</body>
-</html>
