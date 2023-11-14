d400301c7e0e378a64c3af608f4dc59825d0cf8d
vinz0369 <vinhht2k3@gmail.com>
2023-11-14 16:09:38 +0700

    PHP thuc hanh 2

--- Php/index.php
+++ Php/index.php
@@ -7,17 +7,52 @@
     <title>Document</title>
 </head>
 <body>
+	<h1>Currency Converter</h1>
+	<form method="get">
+		<label for="amount">Amount:</label>
+		<input type="number"  name="amount"><br><br>
+		<label for="currency">Select currency:</label>
+		<select  name="currency">
+			<option value="USD">USD</option>
+			<option value="AUD">AUD</option>
+			<option value="JPY">JPY</option>
+			<option value="EUR">EUR</option>
+		</select><br><br>
+		<input type="submit" value="Convert">
+	</form>
 <?php
-    echo"THE GAME";
-    if ($_GET["response"] == "yes")
+    $USD =23000;
+    $AUD =17000;
+    $JPY =200;
+    $EUR =27000;
+    $amount =$_GET['amount'];
+    if ($_GET["currency"]=="USD")
     {
-        echo "HAS BEEN QUITED";
+        echo"$amount USD is equal ";
+
+        echo $amount * $USD;
+    
+
     }
-    if($_GET["response"] == "no")
+    elseif($_GET["currency"] =="AUD")
     {
-        echo"WILL BE CONTINUED IN 3 SECONDS";
+        echo"$amount AUD is equal ";
+
+        echo $amount * $AUD;
     }
-    echo"<br>AFTER IF STATEMENT ";
+    elseif($_GET["currency"] =="JPY")
+    {
+        echo"$amount JPY is equal ";
+
+        echo $amount * $JPY;
+    }
+    else
+    {
+        echo"$amount EUR is equal ";
+
+        echo $amount *$EUR;
+    }
+    echo"VND";
     ?>
 </body>
 </html>
