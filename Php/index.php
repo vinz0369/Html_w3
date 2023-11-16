66e3c43eb3964f7cd9f6dae703bad273245de162
vinz0369 <vinhht2k3@gmail.com>
2023-11-16 22:40:18 +0700

    BTVN 1

--- Php/index.php
+++ Php/index.php
@@ -2,60 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Is it Christmas?</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 50px;
+        }
+        h1 {
+            color: #333;
+        }
+        p{
+            font-size: 200px
+        
+        }
+    </style>
 </head>
 <body>
-	<h1>Du bao trung tuyen</h1>
-	<form method="get">
-		<label for="diemtoan">Điểm toán:</label>
-		<input type="number"  name="diemtoan"><br><br>
-        <label for="diemly">Điểm lý:</label>
-		<input type="number"  name="diemly"><br><br>
-        <label for="diemhoa">Điểm hóa:</label>
-		<input type="number"  name="diemhoa"><br><br>
-	
-		</select><br><br>
-		<input type="submit" value="Kiem tra">
-	</form>
+
 <?php
-    $CNTT =18;
-    $CNTTCLC =22;
-    $KHDL =18;
-    $DC =17;
-    $MT = 15;
-    $diemtoan =$_GET['diemtoan'];
-    $diemly =$_GET['diemly'];
-    $diemhoa =$_GET['diemhoa'];
-    $trungbinh = $diemtoan  + $diemly + $diemhoa;
+    // Kiểm tra xem có phải là Giáng sinh hay không
+    $isChristmas = date('m-d') === '12-25';
+?>
 
-    if ($trungbinh >=$CNTTCLC)
-    {
-        echo"Ban da trung tuyen nganh CNTT_CLC";
+<h1>HÔM NAY LÀ NGÀY GIÁNG SINH ?</h1>
+<?php if ($isChristmas): ?>
+    <p>ĐÚNG</p>
+<?php else: ?>
+    <p>SAI</p>
+<?php endif; ?>
 
-    }
-    elseif($trungbinh >= $CNTT)
-    {
-        echo"Ban da trung tuyen nganh CNTT ";
-
-    }
-    elseif($trungbinh >= $KHDL)
-    {
-        echo"Ban da trung tuyen nganh KHDL ";
-
-    }
-    elseif($trungbinh >= $DC)
-    {
-        echo"Ban da trung tuyen nganh DIA CHAT ";
-
-    }
-    else
-    {
-        echo"Ban da trung tuyen nganh MOI TRUONG ";
-
-    }
- 
-    ?>
 </body>
 </html>
