051ad631b20d6e840bbdc58553d4aa29f41031ce
vinz0369 <vinhht2k3@gmail.com>
2023-11-17 10:25:21 +0700

    slide 28

--- Php/index.php
+++ Php/index.php
@@ -1,8 +1,14 @@
 <?php
 $exchangeRate = array(22300, 27300, 17000, 120);
+$exchangeRate[]= 600;
+$exchangeRate[]= 16000;
+$exchangeRate[0]=22200;
 echo"<h3> Exchange rate to VND </h3>";
 echo "USD: $exchangeRate[0] <br> ";
 echo "EUR: $exchangeRate[1] <br> ";
 echo "SGD: $exchangeRate[2] <br> ";
 echo "JPY: $exchangeRate[3] <br> ";
+echo "CHN: $exchangeRate[4] <br> ";
+echo "AUD: $exchangeRate[5] <br> ";
+
 ?>
