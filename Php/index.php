1a37a48da657c6601f3b2908c2b1bd5b13bf9292
vinz0369 <vinhht2k3@gmail.com>
2023-11-17 10:23:22 +0700

    slide 26

--- Php/index.php
+++ Php/index.php
@@ -1,44 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-	<h1>Kiem tra suc khoe</h1>
-	<form method="get">
-		<label for="chieucao">Chieu cao:</label>
-		<input type="number"  name="chieucao"><br><br>
-        <label for="cannang">Can nang:</label>
-		<input type="number" name="cannang"><br><br>
-		
-        <input type="radio" name="gioitinh" value="nam"> Nam
-        <input type="radio" name="gioitinh" value="nu"> Nu
-
-	
-		</select><br><br>
-		<input type="submit" value="Kiem tra">
-	</form>
 <?php
-    
-    $chieucao =$_GET['chieucao'];
-    $cannang =$_GET['cannang'];
-    $gioitinh =$_GET['gioitinh'];
-
-    if ($gioitinh =="nam" and $cannang >= 50 and $chieucao >=160 )
-    {
-        echo"Ban da dat yeu cau vao cong ty";
-    }
-    elseif($gioitinh =="nu" and $cannang >=40 and $cannang <=70 and $chieucao>=150)
-    {
-        echo"Ban da dat yeu cau vao cong ty";
-    }
-    else
-    {
-        echo"Ban chua dat yeu cau vao cong ty"; 
-    }
-    ?>
-</body>
-</html>
+$exchangeRate = array(22300, 27300, 17000, 120);
+echo"<h3> Exchange rate to VND </h3>";
+echo "USD: $exchangeRate[0] <br> ";
+echo "EUR: $exchangeRate[1] <br> ";
+echo "SGD: $exchangeRate[2] <br> ";
+echo "JPY: $exchangeRate[3] <br> ";
+?>
