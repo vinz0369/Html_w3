9a49b7cbf72eac2010e577b7ebb77e8a1894e166
vinz0369 <vinhht2k3@gmail.com>
2023-11-17 10:46:38 +0700

    slide 33

--- Php/index.php
+++ Php/index.php
@@ -1,11 +1,15 @@
-<?php
-$exchangeRate = array("USD"=>22300,"EUR"=> 27300,"SGD"=> 17000,"JPY"=> 120);
-
-echo"<h3> Exchange rate to VND </h3>";
-echo "USD:" .$exchangeRate['USD']. "<br> ";
-echo "EUR:" .$exchangeRate['EUR']. "<br> ";
-echo "SGD:" .$exchangeRate['SGD']. "<br> ";
-echo "JPY:".$exchangeRate['JPY']. "<br> ";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000, "JPY"=>120);
+        $amount =$_GET['amount'];
+        $currency =$_GET["currency"];
+        echo"$amount USD is equal ".number_format($amount *exchangeRate[$currency]). "VND";
+        ?>
+    </body>
+</html>
