d1e00ff8b03d093f5ac639275f69fa81f43895ac
vinz0369 <vinhht2k3@gmail.com>
2023-11-24 10:57:46 +0700

    slide 16

--- Php/index.php
+++ Php/index.php
@@ -1,8 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Date Listbox</title>
+</head>
+<body>
+
+<form method="get">
+    <label for="day">Ngày:</label>
+    <select name="day" id="day">
+        <?php
+        for ($day = 1; $day <= 31; $day++) {
+            echo "<option value='$day'>$day</option>";
+        }
+        ?>
+    </select>
+
+    <label for="month">Tháng:</label>
+    <select name="month" id="month">
+        <?php
+        for ($month = 1; $month <= 12; $month++) {
+            echo "<option value='$month'>$month</option>";
+        }
+        ?>
+    </select>
+
+    <label for="year">Năm:</label>
+    <select name="year" id="year">
+        <?php
+        $today = getdate();
+    
+        for ($year =$today["year"]; $year >= 1900; $year--) {
+            echo "<option value='$year'>$year</option>";
+        }
+        ?>
+    </select>
+    <input type="submit" value="Submit"> <br>
+</form>
 <?php
-    $x=1;
-    while ($x <=10)
-    {
-        echo" day la so $x <br>";
-        
-    }
-    ?>
+    $day=$_GET["day"];
+    $month=$_GET["month"];
+    $year=$_GET["year"];
+    echo"Ngày bạn lựa chọn là : $day - $month - $year";
+?>
+</body>
+</html>
