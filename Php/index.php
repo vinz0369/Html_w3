482e770167732611077f254bb9fea3864eeb56d4
vinz0369 <vinhht2k3@gmail.com>
2023-11-24 11:16:42 +0700

    Slide 22

--- Php/index.php
+++ Php/index.php
@@ -1,48 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Date Listbox</title>
-</head>
-<body>
-
-<form method="get">
-    <label for="day">Ngày:</label>
-    <select name="day" id="day">
-        <?php
-        for ($day = 1; $day <= 31; $day++) {
-            echo "<option value='$day'>$day</option>";
-        }
-        ?>
-    </select>
-
-    <label for="month">Tháng:</label>
-    <select name="month" id="month">
-        <?php
-        for ($month = 1; $month <= 12; $month++) {
-            echo "<option value='$month'>$month</option>";
-        }
-        ?>
-    </select>
-
-    <label for="year">Năm:</label>
-    <select name="year" id="year">
-        <?php
-        $today = getdate();
-    
-        for ($year =$today["year"]; $year >= 1900; $year--) {
-            echo "<option value='$year'>$year</option>";
-        }
-        ?>
-    </select>
-    <input type="submit" value="Submit"> <br>
-</form>
 <?php
-    $day=$_GET["day"];
-    $month=$_GET["month"];
-    $year=$_GET["year"];
-    echo"Ngày bạn lựa chọn là : $day - $month - $year";
-?>
-</body>
-</html>
+    function img_list($imageList)
+    {
+        foreach ($imageList as $item)
+        {
+            echo "<img src='images/$item'width =50px height = 50px alt ='$item'>";
+
+        }
+    }
+    $items =array ('item1.jpg', 'item2.jpg', 'item3.jpg');
+    img_list($items);
+    ?>
+    <h1>function</h1>
+    <?php
+    $items = array('item4.jpg','item5.jpg');
+    img_list($items);
+    ?>
