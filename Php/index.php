34a6f2be08bfab2fa26cc741d206901a4ce78eec
vinz0369 <vinhht2k3@gmail.com>
2023-12-06 10:11:07 +0700

    slide 60

--- Php/index.php
+++ Php/index.php
@@ -1,12 +1,14 @@
 <?php
-// connect to the database
 require 'connect.php';
-mysqli_set_charset($conn, 'UTF8'); // sửa lỗi tiếng việt
-// // Create sql to select data
-$sql="SELECT * FROM flights";
-// run the query and store result to a variable
-$result = $conn->query($sql);
-print_r($result); // In $result
-// close the connection
-$conn->close();
+mysqli_set_charset($conn, 'UTF8');
+$sql ="SELECT *FORM fligts";
+$result =$conn ->query($sql);
+print_r($result);
+$row =$result ->fetch_assoc();
+echo "<br>";
+print_r($row);
+$row =$result ->fetch_assoc();
+echo "<br>";
+print_r ($row);
+$conn ->close();
 ?>
